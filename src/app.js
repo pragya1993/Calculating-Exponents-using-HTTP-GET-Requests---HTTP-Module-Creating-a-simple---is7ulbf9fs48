00ca79bc31b4aac5b245782e4f21a950b508e8e7
Pragya Pandey <112811665+pragya1993@users.noreply.github.com>
2023-04-11 19:49:39 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,47 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST') {
-    const chunks = [];
-
+  const reqUrl = url.parse(req.url, true);
+  
+  // Check if the request method is POST and the URL is "/"
+  if (req.method === 'POST' && reqUrl.pathname === '/') {
+    let body = '';
+    
+    // Read the request body
     req.on('data', chunk => {
-      const buf = Buffer.from(chunk);
-      const str = buf.toString();
-      chunks.push(str);
-      const obj = JSON.parse(chunks)
-      const value1 = obj.num1;
-      const value2 = obj.num2;
-
-      // Write code here to calculate power of a number
-      if(value1 <= 0){
-        res.statusCode=404;
-        res.end("Not Found");
-      }
-      if(value2 < 0){
-        res.statusCode=200;
-      }
-      
+      body += chunk.toString();
     });
-    }
+    
+    req.on('end', () => {
+      try {
+        const data = JSON.parse(body);
+        
+        // Check if num1 and num2 are valid
+        const num1 = parseInt(data.num1);
+        const num2 = parseInt(data.num2);
+        
+        if (isNaN(num1) || isNaN(num2) || num1 <= 0 || num2 < 0) {
+          res.writeHead(400, {'Content-Type': 'text/plain'});
+          res.end('Invalid input');
+        } else {
+          // Calculate the exponentiation
+          const result = Math.pow(num1, num2);
+          
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          res.end(`The result is ${result}`);
+        }
+      } catch (err) {
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.end('Invalid input');
+      }
+    });
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('Not Found');
+  }
 });
 
-module.exports = server;
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
